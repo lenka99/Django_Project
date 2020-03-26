76e876c8ce1d1bc42f904fc322d576ab29c49c6f
buguwu <lenkabfsu@163.com>
2020-03-25 23:29:22 -0700

    Chubby Cat Insert

--- Insta/views.py
+++ Insta/views.py
@@ -2,7 +2,7 @@ from annoying.decorators import ajax_request
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse, reverse_lazy
-from Insta.models import Post, Like, InstaUser
+from Insta.models import Post, Like, InstaUser, UserConnection
 
 
 from Insta.forms import CustomUserCreationForm
@@ -13,6 +13,13 @@ class HelloWorld(TemplateView):
 class PostsView(ListView):
     model = Post
     template_name = 'index.html'
+
+    def get_queryset(self):
+        current_user = self.request.user
+        following = set()
+        for conn in UserConnection.objects.filter(creator=current_user).select_related('following'):
+            following.add(conn.following)
+        return Post.objects.filter(author__in=following)
 
 class PostDetailView(DetailView):
     model = Post
